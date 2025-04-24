670aa2ef4758f1f5d6d9fdb2498b79224a0ac989
jjspscl <jjspscl@gmail.com>
2025-04-24 23:38:27 +0800

    ⚡ZSHRC: zsh-autosuggest, volta

---
+++ Hooks/zshrc/pre.sh
@@ -0,0 +1,6 @@
+
+# ZSH-AUTOSUGGESTION
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# volta
+curl https://get.volta.sh | bash
