cb323787ecea4c11344f20ea2c3cac347c6380f7
jjspscl <jjspscl@gmail.com>
2026-03-30 09:56:09 +0800

    feat: add neovim and opencode configs
    - neovim: kickstart-based, kanagawa dragon transparent, lazy.nvim
      LSP: tsserver, denols, basedpyright, ruff, gopls, lua_ls
      Plugins: telescope, blink.cmp, treesitter, neo-tree, gitsigns,
      which-key, mini.nvim, copilot, tmux.nvim, conform, DAP
    - opencode: 5-agent config (build, plan, tester, PM, code-reviewer)
      kanagawa-transparent theme, tamagui skill

---
+++ Configs/nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<C-b>', ':Neotree toggle<CR>', desc = 'Toggle NeoTree (VS Code style)', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- show hidden files dimmed
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+      window = {
+        mappings = {
+          ['<C-b>'] = 'close_window',
+        },
+      },
+    },
+  },
+}
